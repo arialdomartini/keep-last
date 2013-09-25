67fe0fb52e86884932193381c91fa9a87d79625d
Arialdo Martini <arialdomartini@gmail.com>
2013-09-25 22:05:00 +0200

    --help shows a help message

--- keep-last.py
+++ keep-last.py
@@ -1,2 +1,37 @@
 #!/usr/bin/python
-pass
+import sys
+import getopt
+import os
+
+class RunException(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+class Usage(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+
+def main(argv=None):
+
+    if argv is None:
+        argv = sys.argv
+  
+    try:
+        try:
+            opts, args = getopt.getopt(argv[1:], "h", ["help"])
+        except getopt.error, msg:
+            raise Usage(msg)
+
+        for o, a in opts:
+            if o in ("-h", "--help"):
+                raise Usage("""Usage:
+%s [--help]""" % os.path.basename(sys.argv[0]))
+
+    except Usage, err:
+        print >>sys.stderr, err.msg
+        print >>sys.stderr, "for help use --help"
+        return 2
+
+if __name__ == "__main__":
+    sys.exit(main())
